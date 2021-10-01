51f2c5154e775ded253159f9de7413b692490b84
Thomas <kaulketh@gmail.com>
2021-10-01 16:06:43 +0200

    update config

--- config/gpio.py
+++ config/gpio.py
@@ -6,3 +6,16 @@
 # Thomas Kaulke, kaulketh@gmail.com
 # https://github.com/kaulketh
 # -----------------------------------------------------------
+import RPi.GPIO as GPIO
+
+PIN = 23
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(PIN, GPIO.IN)
+
+
+def switch_state():
+    return False if GPIO.input(PIN) == 0 else True
+
+
+if __name__ == '__main__':
+    pass
